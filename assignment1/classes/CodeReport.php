ce82af389186dc042a2cb4413d4587ad80dd8428
Aleksei Papulovskii <aleksei.papulovskii@booking.com>
2015-10-25 18:35:10 +0100

    OOish code solution

---
+++ assignment1/classes/CodeReport.php
@@ -0,0 +1,87 @@
+<?php
+class CodeReport extends Report {
+    /*
+     * Here it might be a good idea to split this "huge" method to several,
+     * but I think it's okay to do in case of growing complexity and not now.
+     */
+    public function run() {
+        $report   = [];
+        $bookers  = [];
+        $bookings = [];
+        $items    = [];
+
+
+        // Load all bookings
+        $bookings_raw = $this->db->prepare('SELECT * FROM bookings')->run();
+        foreach($bookings_raw as $b) {
+            $bookings[$b->id] = $b;
+        }
+
+
+        // Load all bookingitems
+        $items_raw = $this->db->prepare('SELECT * FROM bookingitems')->run();
+        foreach($items_raw as $item) {
+            $booking = $bookings[$item->booking_id];
+            if(!isset($bookers[$booking->booker_id])) {
+                $bookers[$booking->booker_id] = [
+                    'first_booking' => null,
+                    'bookings'      => [],
+                    'sum'           => []
+                ];
+            }
+            $booker = &$bookers[$booking->booker_id];
+
+            // Saving first booking
+            if($booker['first_booking'] > $item->end_timestamp || is_null($booker['first_booking'])) {
+                $booker['first_booking'] = $item->end_timestamp;
+            }
+
+            // I'd consider here that "average number of bookings" in the report is actually number of bookings, not items
+            $month = (int) date("Ym", $item->end_timestamp); // month here is for grouping by months
+            $booker['bookings'][$month][$booking->id] = 1;
+            if(!isset($booker['sum'][$month])) {
+                $booker['sum'][$month] = 0;
+            }
+            $booker['sum'][$month] += $item->locked_total_price;
+        }
+
+
+        // Mix it!
+        $today = (int) date("Ym");
+        foreach($bookers as $id => $booker) {
+            $first_month  = (int) date("Ym", $booker['first_booking']);
+            if($today - $first_month < $this->period) {
+                continue;
+            }
+            $report_month = date("Y-m", $booker['first_booking']);
+            $booker_bookings = 0;
+            $booker_sum = 0;
+            // Filtering only bookings for period
+            foreach($booker['bookings'] as $month => $bkngs) {
+                if($month - $first_month <= $this->period) {
+                    $booker_bookings += count($bkngs);
+                    $booker_sum += $booker['sum'][$month];
+                }
+            }
+            $report[$report_month]['bookers'][$id] = [ 'bookings' => $booker_bookings, 'sum' => $booker_sum ];
+        }
+
+
+        // Generate a report
+        $result = [];
+        $rows = array_keys($report);
+        sort($rows);
+        foreach($rows as $month) {
+            $bookers_count  = count($report[$month]['bookers']);
+            $bookings_count = array_sum(array_map(function($i) { return $i['bookings']; }, $report[$month]['bookers']));
+            $bookings_sum   = array_sum(array_map(function($i) { return $i['sum']; }, $report[$month]['bookers']));
+            $this->add(
+                        $month,
+                        $bookers_count,
+                        $bookers_count  > 0 ? sprintf("%.2f", $bookings_count/$bookers_count) : 0,
+                        $bookings_count > 0 ? sprintf("%.2f", $bookings_sum/$bookings_count) : 0,
+                        $bookings_count > 0 ? sprintf("%.2f", $this->commission * $bookings_sum/$bookings_count) : 0
+            );
+        }
+    }
+}
--- assignment1/index.php
+++ assignment1/index.php
@@ -5,6 +5,7 @@ require_once(__DIR__ . '/errors.php');
 require_once(__DIR__ . '/include.php');
 require_once(__DIR__ . '/classes/Month.php');
 require_once(__DIR__ . '/classes/Report.php');
+require_once(__DIR__ . '/classes/CodeReport.php');
 
 date_default_timezone_set('Europe/Amsterdam');
 
@@ -23,79 +24,9 @@ if(isset($_GET['commission']) && in_array($_GET['commission'], $commissions)) {
 	$commission = (float) $_GET['commission'];
 }
 
-$report   = [];
-$bookers  = [];
-$bookings = [];
-$items    = [];
-// Load all bookings
-$bookings_raw = $db->prepare('SELECT * FROM bookings')->run();
-foreach($bookings_raw as $b) {
-	$bookings[$b->id] = $b;
-	// $bookers[$b->booker_id]['bookings'][] = $b->id;
-}
+$result = new CodeReport($db, $period, $commission);
+$result->run();
 
-// Load all bookingitems
-$items_raw = $db->prepare('SELECT * FROM bookingitems')->run();
-foreach($items_raw as $item) {
-	$booking = $bookings[$item->booking_id];
-	if(!isset($bookers[$booking->booker_id])) {
-		$bookers[$booking->booker_id] = [
-			'first_booking' => null,
-			'bookings'      => [],
-			'sum'           => []
-		];
-	}
-	$booker = &$bookers[$booking->booker_id];
-
-	// Saving first booking
-	if($booker['first_booking'] > $item->end_timestamp || is_null($booker['first_booking'])) {
-		$booker['first_booking'] = $item->end_timestamp;
-	}
-
-	// I'd consider here that "average number of bookings" in the report is actually number of bookings, not items
-	$month = (int) date("Ym", $item->end_timestamp); // month here is for grouping by months
-	$booker['bookings'][$month][$booking->id] = 1;
-	if(!isset($booker['sum'][$month])) {
-		$booker['sum'][$month] = 0;
-	}
-	$booker['sum'][$month] += $item->locked_total_price;
-}
-// Mix it!
-$today = (int) date("Ym");
-foreach($bookers as $id => $booker) {
-	$first_month  = (int) date("Ym", $booker['first_booking']);
-	if($today - $first_month < $period) {
-		continue;
-	}
-	$report_month = date("Y-m", $booker['first_booking']);
-	$booker_bookings = 0;
-	$booker_sum = 0;
-	// Filtering only bookings for period
-	foreach($booker['bookings'] as $month => $bkngs) {
-		if($month - $first_month <= $period) {
-			$booker_bookings += count($bkngs);
-			$booker_sum += $booker['sum'][$month];
-		}
-	}
-	$report[$report_month]['bookers'][$id] = [ 'bookings' => $booker_bookings, 'sum' => $booker_sum ];
-}
-// Generate a report
-$result = [];
-$rows = array_keys($report);
-sort($rows);
-foreach($rows as $month) {
-	$bookers_count  = count($report[$month]['bookers']);
-	$bookings_count = array_sum(array_map(function($i) { return $i['bookings']; }, $report[$month]['bookers']));
-	$bookings_sum   = array_sum(array_map(function($i) { return $i['sum']; }, $report[$month]['bookers']));
-	$result[] = [
-		'month'    => $month,
-		'label'    => date("M Y", strtotime($month)),
-		'bookers'  => $bookers_count,
-		'bookings' => $bookers_count  > 0 ? sprintf("%.2f", $bookings_count/$bookers_count) : 0,
-		'turnover' => $bookings_count > 0 ? sprintf("%.2f", $bookings_sum/$bookings_count) : 0,
-		'LTV'      => $bookings_count > 0 ? sprintf("%.2f", $commission * $bookings_sum/$bookings_count) : 0,
-	];
-}
 
 ?>
 <!doctype html>
@@ -146,11 +77,11 @@ foreach($rows as $month) {
 			<tbody>
 				<?php foreach ($result as $index => $row): ?>
 					<tr>
-						<td><?php echo $row['label']?></td>
-						<td><?php echo $row['bookers']?></td>
-						<td class="right"><?php echo $row['bookings']?></td>
-						<td class="right"><?php echo $row['turnover']?></td>
-						<td class="right"><?php echo $row['LTV']?></td>
+						<td><?php echo $row->label;?></td>
+						<td><?php echo $row->bookers;?></td>
+						<td class="right"><?php echo $row->bookings;?></td>
+						<td class="right"><?php echo $row->turnover;?></td>
+						<td class="right"><?php echo $row->LTV;?></td>
 					</tr>
 				<?php endforeach; ?>
 			</tbody>
